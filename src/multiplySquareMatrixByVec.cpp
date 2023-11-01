5bc81f139dfd00aac3508bdbf21ea1f5b3362ea2
stocktonsmith <103328822+stocktonsmith@users.noreply.github.com>
2023-10-31 19:45:05 -0600

    Update and rename multiplySquareMatrixByOnes.cpp to multiplySquareMatrixByVec.cpp

--- src/multiplySquareMatrixByVec.cpp
+++ src/multiplySquareMatrixByVec.cpp
@@ -1,6 +1,7 @@
 #include "mathRoutines.hpp"
 
-std::vector<double> multiplySquareMatrixByOnes(const std::vector<std::vector<double>>& matrix)
+std::vector<double> multiplySquareMatrixByVec(const std::vector<std::vector<double>>& matrix,
+	const std::vector<double>& rhs)
 {
 	std::vector<double> rhsToReturn(matrix.size());
 	for (size_t rowNum = 0; rowNum < matrix.size(); rowNum++)
@@ -8,7 +9,7 @@ std::vector<double> multiplySquareMatrixByOnes(const std::vector<std::vector<dou
 		double total = 0;
 		for (size_t colNum = 0; colNum < matrix.size(); colNum++)
 		{
-			total += matrix[rowNum][colNum];
+			total += matrix[rowNum][colNum] * rhs[colNum];
 		}
 		rhsToReturn[rowNum] = total;
 	}
